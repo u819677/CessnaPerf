388984aa3707f80dae198c8f265b19c78a2cb5e3
Richard Clark <clarkrd@btinternet.com>
2023-05-01 09:21:47 +0100

    Initial Commit

---
+++ CessnaPerf/CessnaPerfApp.swift
@@ -0,0 +1,17 @@
+//
+//  CessnaPerfApp.swift
+//  CessnaPerf
+//
+//  Created by Richard Clark on 01/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CessnaPerfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
